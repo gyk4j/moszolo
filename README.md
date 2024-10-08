5a76b82383be10056e44686c85e4293274a604f1
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-10-08 10:09:47 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # o2010s
 PowerShell script to download the free Microsoft Office 2010 Starter as a locally-cached copy for reuse or archival.
+
+Inspired and rewritten from 
+[Office 2010 Starter downloader AutoIt script by Jemboy][downloader].
+
+Written for learning and practising [AutoIt][autoit] and [PowerShell][pwsh] 
+scripting.
+
+# How it works
+
+Office 2010 Starter Edition is packaged using the [Microsoft App-V][ms-app-v] 
+virtualization technology to enable it to be downloaded and run on-demand 
+without prior installation.
+
+`o2010s` is a PowerShell script that downloads and parses the App-V's manifest 
+and catalog files in XML format to identify and download the appropriate 
+required files for Office 2010 Starter Edition to install and run successfully.
+
+The downloaded files can be archived and preserved as a local copy for future 
+reuse.
+
+# Caution Against Using Microsoft Office 2010
+
+> [!CAUTION]
+>
+> There is NO REASON to use Microsoft Office 2010 today.
+>
+> It is a *seriously outdated and unmaintained* software, with no security 
+> updates and perhaps plenty of security vulnerabilities ever since support 
+> ended on **October 13, 2020**.
+
+# Recommendations
+
+- Use [Office 365][office-365] with a [free Microsoft account][ms-acct] if you 
+  can.
+- If you insist on using Microsoft Office 2010 for whatever reason, use it only
+  from within a virtual machine and/or without Internet connectivity.
+
+# Usage
+
+*o2010s* only requires *PowerShell 3.0* or above. Higher versions of PowerShell
+are pre-installed with modern versions of Windows in use. Windows 10 and 11 
+come with *PowerShell 5.1* out-of-the-box, so no further action is required.
+
+From `PowerShell`
+
+```pwsh
+.\o2010s-downloader.ps1
+```
+
+From `Command Prompt`
+
+```bat
+powershell.exe o2010s-downloader.ps1
+```
+
+# Settings and Customizations
+
+There are some variables and constants in the PowerShell script that allow users
+to specify custom settings before executing the script. They include:
+
+- `$DEBUG`: Runs in debug mode which simulates and skips downloading files from 
+  servers while reusing existing files already on storage. Used only for 
+  testing during development only. (default: `false`)
+- `$lang`: Downloads the "English-US" version by default. (default: `en-us`)
+  Languages still available for download include:
+  - ar-sa (Arabic)
+  - da-dk (Danish)
+  - de-de (German)
+  - en-us (English)
+  - es-es (Spanish)
+  - fr-fr (French)
+  - he-il (Hebrew)
+  - hi-in (Hindi)
+  - it-it (Italian)
+  - ja-jp (Japanese)
+  - ko-kr (Korean)
+  - nl-nl (Dutch)
+  - pl-pl (Polish)
+  - pt-br (Portuguese)
+  - ru-ru (Russian)
+  - sv-se (Swedish)
+  - th-th (Thai)
+  - zh-cn (Chinese Simplified)
+  - zh-tw (Chinese Traditional)
+
+[downloader]: https://www.autoitscript.com/forum/topic/205471-office-2010-starter-downloader/
+[autoit]: https://www.autoitscript.com/site/
+[pwsh]: https://learn.microsoft.com/en-us/powershell/
+[ms-app-v]: https://en.wikipedia.org/wiki/Microsoft_App-V
+[office-365]: https://www.office.com/
+[ms-acct]: https://account.microsoft.com/account/Account
---
+++ o2010s-downloader.ps1
@@ -0,0 +1,151 @@
+﻿[bool]$DEBUG = $false
+
+[string]$lang = 'ja-jp'
+[string]$version = '14.0.4763.1000'
+
+function Download-Urls( [Collections.Generic.List[string]]$Urls ){
+    [string[]]$skipBigFile = @()
+    # ".sft", ".dsft", ".msi", ".exe", ".dll", ".cab"
+
+    $urls | ForEach-Object {
+        if(!$DEBUG){
+            [string]$url = $_;
+            [string]$filename = Split-Path $_ -Leaf
+            
+
+            [string]$extension = [System.IO.Path]::GetExtension($filename)
+
+            
+            if($skipBigFile.Contains($extension.ToLower())){
+                $filename
+                New-Item -ItemType File -Name $filename -Force | Out-Null
+            }
+            else{
+                $url
+                Invoke-WebRequest -Uri $url -OutFile $filename
+            }
+        }
+        else{
+            [string]$path = $_.Replace("http://", "..\..\").Replace("/", "\")
+            [string]$filename = Split-Path $_ -Leaf
+
+            if(Test-Path -Path $path){
+                $path
+                Copy-Item -Path $path -Destination $filename
+            }
+            else{
+                $filename
+                New-Item -ItemType File -Name $filename -Force | Out-Null
+            }
+        }
+    }
+    $urls.Clear()
+}
+
+function Parse-Xml( [string]$XmlFile, [string]$XPath, [string]$Attribute, [Collections.Generic.List[string]]$DownloadQueue ){
+    if(Test-Path -Path $XmlFile){
+        Select-Xml -Path $XmlFile -XPath $XPath | ForEach-Object { 
+            $DownloadQueue.Add($_.Node.Attributes[$Attribute].Value)
+        }
+    }
+    else{
+        [string]::Format("Error: {0} is missing", $XmlFile)
+    }
+}
+
+function Extract-Cabinet( [string]$Cab, [string]$Directory ){
+    if((Test-Path -Path $Cab) -and (Get-Item $Cab).Length -gt 0){
+        "Extracting to " + $Directory
+        Start-Process -Wait -NoNewWindow -FilePath "C:\Windows\System32\extrac32.exe" -ArgumentList "/Y", "/A", $Cab, "/E", "/L", $Directory
+    }
+    else{
+        "Skipped " + $Cab + ". File does not exist or it is empty."
+    }
+}
+
+function Write-Readme(){
+    [string]$content = @'
+To install Office 2010 Starter, go to into the 'bin' folder and to the right language directory.
+E.g. '.../bin/de-de' is the folder for the German Office 2010 Starter installation files.
+ 
+1. Run SetupConsumerC2ROLW.exe with right mouse click 'Run as Administrator'
+2. After the installation, double click click2run-x-none.msp (KB2598285) within de update folder and restart
+   your computer. https://www.catalog.update.microsoft.com/Search.aspx?q=2598285
+ 
+After the above reboot, Office 2010 Starter will work on Windows 10 20H2.
+ 
+To copy the shortcuts to your desktop:
+a. Go to search and type 'Word'
+b. At 'Microsoft Word Starter 2010', click on 'Open file location'
+c. Now copy the shortcuts (Word and Excel) to your desktop.
+ 
+Office 2010 Starter works with a virtualization technology, which creates a 
+system drive Q:. If you are already using drive Q: then Office 2010 Starter won't start.
+ 
+Office 2010 starter files have been unavailable for some time, but Microsoft quietly 
+activated the downloads again a few years ago.
+
+'@
+
+    $content | Out-file -FilePath "README.TXT"
+    "README.TXT"
+}
+
+[string]$scriptDir = $PSCommandPath | Split-Path -Parent
+[string]$downloadBaseDir = [string]::Format("{0}\bin\{1}", $scriptDir, $lang)
+
+New-Item -Path $downloadBaseDir -ItemType Directory -Force | Out-Null
+Set-Location $downloadBaseDir
+
+[string]$remoteBaseUrl = [string]::Format("http://c2r.microsoft.com/consumerC2R/{0}/{1}/", $lang, $version)
+[string]$localBasePath = [string]::Format(".\c2r.microsoft.com\ConsumerC2R\{0}\{1}\", $lang, $version)
+[string]$windowsUpdateCatalogBaseUrl = "http://download.windowsupdate.com/d/msdownload/update/software/crup/"
+
+[Collections.Generic.List[string]]$downloadQueue = New-Object Collections.Generic.List[string]
+
+"--- Phase 1: Downloading base files..."
+$downloadQueue.Add($remoteBaseUrl + "setupconsumerc2rolw.exe")
+$downloadQueue.Add($remoteBaseUrl + "PackageProperties.xml")
+
+Download-Urls -Urls $downloadQueue
+
+"--- Phase 2: Downloading package..."
+
+Parse-Xml -XmlFile "PackageProperties.xml" `
+    -XPath '/PackageProperties/Files/File' `
+    -Attribute "ServerLocation" `
+    -DownloadQueue $downloadQueue
+
+Download-Urls -Urls $downloadQueue
+
+"--- Phase 3: Downloading differential SFT files..."
+
+Parse-Xml -XmlFile "descriptor.xml" `
+    -XPath '/ToothpickDescriptor/DiffSftList/File' `
+    -Attribute "Url" `
+    -DownloadQueue $downloadQueue
+
+Download-Urls -Urls $downloadQueue
+
+"--- Phase 4: Downloading Office updates..."
+
+# KB2598285 (Sep 18, 2013)
+$downloadQueue.Add($windowsUpdateCatalogBaseUrl + "2013/09/click2run-x-none_f74703316deaa94b7b7e72bfcf7bd718910e26a4.cab")
+                    
+# KB2837578 (Apr 14, 2015)
+$downloadQueue.Add($windowsUpdateCatalogBaseUrl + "2015/04/click2run-x-none_235e9328493ac7313f6862e502a8b4b0a14e8066.cab")
+
+# KB2986257 (May 12, 2015)
+$downloadQueue.Add($windowsUpdateCatalogBaseUrl + "2015/05/click2run-x-none_943e2c5aea7957af886fd3700268c72750513797.cab")
+
+Download-Urls -Urls $downloadQueue
+
+"--- Phase 5: Extracting update cabinet files"
+Extract-Cabinet -Cab "click2run-x-none_f74703316deaa94b7b7e72bfcf7bd718910e26a4.cab" -Directory "KB2598285_20130918"
+Extract-Cabinet -Cab "click2run-x-none_235e9328493ac7313f6862e502a8b4b0a14e8066.cab" -Directory "KB2837578_20150414"
+Extract-Cabinet -Cab "click2run-x-none_943e2c5aea7957af886fd3700268c72750513797.cab" -Directory "KB2986257_20150512"
+
+"--- Phase 6: Writing information files"
+Write-Readme
+
+"--- Done"
